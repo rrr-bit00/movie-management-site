a27ec16ef7be338908f9c8009ac6dc2a12107607
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-11-27 13:52:21 +0900

    WIP: header front

---
+++ frontend/src/app/Layouts/PublicHeader.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+
+export default function PublicHeader() {
+  return (
+    <div>
+        <header className=""></header>
+    </div>
+  )
+}
