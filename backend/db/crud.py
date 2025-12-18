c3a73c0a732d752051e30d3dc39f3fb1bc493a85
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-12-19 00:13:47 +0900

    feat: SQLAlchemyからSQLmodelに変更

---
+++ backend/db/crud.py
@@ -0,0 +1,35 @@
+from sqlmodel import Session
+from models import Movie
+from schemas import MovieCreate, MovieUpdate
+
+def create_movie(movie: MovieCreate, session: Session):
+    db_movie = Movie(**movie.model_dump())
+    session.add(db_movie)
+    session.commit()
+    session.refresh(db_movie)
+    return db_movie
+
+def get_all_movies(session: Session):
+    return session.query(Movie).all()
+
+def get_movie(movie_id: int, session: Session):
+    return session.query(Movie).filter(Movie.id == movie_id).first()
+
+def update_movie(movie_id: int, movie_data: MovieUpdate, session: Session):
+    db_movie = session.query(Movie).filter(Movie.id == movie_id).first()
+    if db_movie is None:
+        return None
+    for key, value in movie_data.model_dump().items():
+        setattr(db_movie, key, value)
+
+    session.commit()
+    session.refresh(db_movie)
+    return db_movie
+
+def delete_movie(movie_id: int, session: Session):
+    db_movie = session.query(Movie).filter(Movie.id == movie_id).first()
+    if db_movie is None:
+        return None
+    session.delete(db_movie)
+    session.commit()
+    return db_movie
