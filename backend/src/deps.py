7a2ae6df5352004e04d9e7b2843108ead2e53f1a
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2026-01-23 01:59:28 +0900

    WIP: アカウント認証やアカウント関連のDB処理を実装中

---
+++ backend/src/deps.py
@@ -0,0 +1,44 @@
+from typing import Annotated
+
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
+
+import jwt
+from fastapi import Depends, HTTPException
+from src.core import security
+from src.core.database import SessionDep
+from src.models.users import User
+
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_STR}/login/access-token"
+)
+
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
+
+
+# 認証を通してユーザーを取得する関数
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
+    # 認証を検証する
+    try:
+        payload = jwt.decode(token, setting.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        token_data = TokenPayload(**payload)
+    # 検証に失敗した場合、403のステータスコードを返す
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="認証情報を検証できませんでした",
+        )
+    # カレントuserを取得
+    user = session.get(User, token_data.sub)
+    # userがなければ404を返す
+    if not user:
+        raise HTTPException(status_code=404, detail="アカウントが見つかりませんでした")
+    # userがあっても、activeじゃなければ400を返す
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="アカウントが有効ではありません")
+    # 全て通れば、userを返す
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
