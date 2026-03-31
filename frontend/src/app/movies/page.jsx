d32d3e818a49414de535705a590804c3ad15830a
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2026-04-01 01:47:33 +0900

    ref: ユーザーのログイン・登録画面と映画を表示するページのスタイルを変更

--- frontend/src/app/movies/page.jsx
+++ frontend/src/app/movies/page.jsx
@@ -10,8 +10,10 @@ export default async function MoviePage({ searchParams }) {
     const movies = await searchMovies(query)
 
     return (
-        <div className="flex flex-wrap justify-center">
-            <MovieList movies={movies} />
-        </div >
+        <div className="min-h-screen bg-[radial-gradient(circle_at_80%_40%,_#f6d1a5,_transparent_25%),radial-gradient(circle_at_30%_70%,_#c9e6ff,_transparent_25%),linear-gradient(345deg,_#0f1218,_#192333_55%,_#12161d)] py-10">
+            <div className="flex flex-wrap justify-center">
+                <MovieList movies={movies} />
+            </div >
+        </div>
     )
 }
