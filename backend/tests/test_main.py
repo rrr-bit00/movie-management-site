34e9905230c78280ef4de96987ce7588a047f0fe
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2026-04-09 03:32:19 +0900

    fix: postテスト時のルーティングを修正

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,13 +1,17 @@
 from fastapi.testclient import TestClient
 
+
 def test_create_movie(client: TestClient):
     # Create
-    r = client.post("/movies", json={
-        "title": "test1",
-        "description": "lorem",
-        "director": "John",
-        "released_year": 2020
-        })
+    r = client.post(
+        "/movies/",
+        json={
+            "title": "test1",
+            "description": "lorem",
+            "director": "John",
+            "released_year": 2020,
+        },
+    )
 
     assert r.status_code in (200, 201)
 
@@ -18,13 +22,13 @@ def test_create_movie(client: TestClient):
     # Read
     r = client.get(f"/movies/{movie_id}")
     assert r.status_code == 200
-    assert r.json()['title'] == "test1"
+    assert r.json()["title"] == "test1"
 
     # Update
-    r = client.put(f"/movies/{movie_id}", json={
-        "title": "test-update",
-        "description": "lorem2",
-        "director": "Anna"})
+    r = client.put(
+        f"/movies/{movie_id}",
+        json={"title": "test-update", "description": "lorem2", "director": "Anna"},
+    )
     assert r.status_code == 200
     assert r.json()["title"] == "test-update"
 
