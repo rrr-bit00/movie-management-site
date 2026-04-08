4c358bee7511a3df71ad0cd4e8e229401799cbcc
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2026-04-09 02:14:29 +0900

    fix: conftestのimportエラーを修正

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -3,12 +3,14 @@ from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
-from main import app
-from db.database import get_session
+from src.main import app
+from src.core.database import get_session
 
 sqlite_url = "sqlite://"
-test_engine = create_engine(sqlite_url, connect_args={"check_same_thread": False},
-                       poolclass=StaticPool)
+test_engine = create_engine(
+    sqlite_url, connect_args={"check_same_thread": False}, poolclass=StaticPool
+)
+
 
 @pytest.fixture()
 def client():
